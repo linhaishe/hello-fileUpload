c8b5c2fc6ef81ff2bff0d3f81c7b88729fb3a2e2
linhaishe <42030043+linhaishe@users.noreply.github.com>
2021-11-11 00:46:35 +0800

    上传文件大小或者格式等限制处理

--- fileUpload/upload.js
+++ fileUpload/upload.js
@@ -1,3 +1,4 @@
+/** 
 //const axios = require('axios').default;
 const { default: axios } = require('axios');
 
@@ -57,3 +58,63 @@ const { default: axios } = require('axios');
       },
     };
 });
+*/
+
+(function () {
+  let upload = document.querySelector('#upload1'),
+    upload_inp = upload.querySelector('.upload_inp'),
+    upload_button_select = upload.querySelector('.upload_button.select'),
+    upload_button_upload = upload.querySelector('.upload_button.upload'),
+    upload_tip = upload.querySelector('.upload_tip'),
+    upload_list = upload.querySelector('.upload_list');
+
+  //移除按钮的点击处理
+  upload_list.addEventListener('click', function (ev) {
+    // console.log('dianjidianji', ev);
+
+    let target = ev.target.tagName;
+    console.log('dianjidianji', target);
+
+    if (target === 'EM') {
+      //点击的是移除按钮
+      console.log('dianjidianjiEMMMMMM');
+      upload_tip.style.display = 'block';
+      upload_list.style.display = 'none';
+      upload_list.innerHTML = ``;
+    }
+  });
+  //监听用户选择文件的操作
+  upload_inp.addEventListener('change', function () {
+    //获取用户选中的文件对象
+    //+ name:文件名
+    //+ size:文件大小B
+    //+ type: 文件的MIME类型
+    let file = upload_inp.files[0];
+
+    if (!file) return;
+    //限制文件上传的格式「方案-」
+    /** 
+    if (!/(PNG |JPG |JPEG)/i.test(file.type)) {
+      alert('.上传的文件只能是PNG/JPG/JPEG格式的');
+      return;
+    }
+    */
+
+    //限制文件上传的大小
+    if (file.size > 2 * 1024 * 1024) {
+      alert('.上传的文件不能超过2MB~~');
+      return;
+    }
+
+    upload_tip.style.display = 'none';
+    upload_list.style.display = 'block';
+    upload_list.innerHTML = `   <li>
+              <span>文件: ${file.name}</span>
+              <span><em>移除</em></span>
+            </li>`;
+  });
+
+  upload_button_select.addEventListener('click', function () {
+    upload_inp.click();
+  });
+})();
