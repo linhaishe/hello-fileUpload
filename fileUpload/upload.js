35b649b6f059e5d8dfefa285691ef01e562c652a
linhaishe <42030043+linhaishe@users.noreply.github.com>
2021-11-15 23:26:44 +0800

    图片多选

--- fileUpload/upload.js
+++ fileUpload/upload.js
@@ -63,6 +63,9 @@ const { default: axios } = require('axios');
 //#endregion
 // import http from './instance';
 // const FormData = require('form-data');
+
+//form-data 单文件上传
+
 (function () {
   let upload = document.querySelector('#upload1'),
     upload_inp = upload.querySelector('.upload_inp'),
@@ -92,6 +95,9 @@ const { default: axios } = require('axios');
           alert(
             `文件已经上传成功~,您可以基于${res.data.servicePath}访问这个资源~~^ `
           );
+          upload_tip.style.display = 'block';
+          upload_list.style.display = 'none';
+          upload_list.innerHTML = ``;
           return;
         }
         return Promise.reject(res.data.codeText);
@@ -145,6 +151,110 @@ const { default: axios } = require('axios');
               <span>文件: ${file.name}</span>
               <span><em>移除</em></span>
             </li>`;
+  });
+
+  upload_button_select.addEventListener('click', function () {
+    upload_inp.click();
+  });
+});
+
+//多文件上传
+(function () {
+  let upload = document.querySelector('#upload2'),
+    upload_inp = upload.querySelector('.upload_inp'),
+    upload_button_select = upload.querySelector('.upload_button.select'),
+    upload_button_upload = upload.querySelector('.upload_button.upload'),
+    upload_tip = upload.querySelector('.upload_tip'),
+    upload_list = upload.querySelector('.upload_list');
+
+  let _files = [];
+
+  //上传文件事件处理
+  upload_button_upload.addEventListener('click', function () {
+    if (_files.length === 0) {
+      alert('请上传文件');
+      return;
+    }
+    //把文件传递给服务器Formbata / BASE64
+
+    let formData = new FormData();
+    formData.append('file', _file);
+    formData.append('filename', _file.name);
+    instance
+      .post('/upload_single', formData)
+      .then((res) => {
+        console.log(res.data.code);
+        if (res.data.code === 0) {
+          alert(
+            `文件已经上传成功~,您可以基于${res.data.servicePath}访问这个资源~~^ `
+          );
+          upload_tip.style.display = 'block';
+          upload_list.style.display = 'none';
+          upload_list.innerHTML = ``;
+          return;
+        }
+        return Promise.reject(res.data.codeText);
+      })
+      .catch((reason) => {
+        alert('文件上传失败，请您稍后再试~~');
+      });
+  });
+
+  //移除按钮的点击处理
+  upload_list.addEventListener('click', function (ev) {
+    // console.log('dianjidianji', ev);
+
+    let target = ev.target;
+    let curLi = null;
+    let key;
+
+    if (target.tagName === 'EM') {
+      //点击的是移除按钮
+      curLi = target.parentNode.parentNode;
+      if (curLi) {
+        key = curLi.getAttribute('key');
+        upload_list.removeChild(curLi);
+        _files = _files.filter((item) => item.key !== key);
+        console.log(_files);
+      }
+
+      // if (!curLi) return;
+      // upload_list.removeChild(curLi);
+    }
+  });
+
+  const createRandom = () => {
+    let ran = Math.random() * new Date();
+    return ran.toString(16).replace('.', '');
+  };
+  //
+  //监听用户选择文件的操作,点击后页面显示
+  upload_inp.addEventListener('change', function () {
+    //获取并存放所有文件
+    _files = Array.from(upload_inp.files);
+    if (_files.length === 0) return;
+
+    _files = _files.map((file) => {
+      return {
+        file,
+        filename: file.name,
+        key: createRandom(),
+      };
+    });
+    console.log('_files', _files);
+
+    let str = ``;
+    _files.forEach((item, index) => {
+      str += `
+      <li key=${item.key}>
+              <span>文件${index + 1}:${item.filename}</span>
+              <span><em>移除</em></span>
+            </li> 
+      `;
+
+      upload_list.innerHTML = str;
+      upload_list.style.display = 'block';
+    });
   });
 
   upload_button_select.addEventListener('click', function () {
